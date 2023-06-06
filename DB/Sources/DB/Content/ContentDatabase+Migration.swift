sitedata/feditext

commit 3ac900f2d614f47aa814b3189ce07bab4df7dadd

Fully implement report notifications
Now displays a summary of the report in the notifications view.

Changes the way instance rules are fetched and stored so they can be joined on in queries.

DB/Sources/DB/Content/ContentDatabase+Migration.swift
@@ -342,6 +342,39 @@ extension ContentDatabase {
             }
         }
 
+        migrator.registerMigration("1.7.4-reports-phase-2") { db in
+            try db.alter(table: "instanceRecord") { t in
+                t.drop(column: "rules")
+            }
+
+            try db.create(table: "rule") { t in
+                t.column("id", .text).primaryKey(onConflict: .replace)
+                t.column("text", .text).notNull()
+            }
+
+            // Note: it's possible for a report notification to refer to statuses or rules that no longer exist,
+            // so we don't enforce a foreign key constraint on `ruleId`.
+
+            try db.create(table: "reportRuleJoin") { t in
+                t.column("reportId", .text).indexed().notNull()
+                    .references("reportRecord", onDelete: .cascade)
+                t.column("ruleId", .text).indexed().notNull()
+
+                t.primaryKey(["reportId", "ruleId"], onConflict: .replace)
+            }
+
+            try db.alter(table: "reportRecord") { t in
+                t.add(column: "actionTakenAt", .date)
+                t.add(column: "category", .text).notNull()
+                t.add(column: "comment", .text).notNull()
+                t.add(column: "forwarded", .boolean).notNull()
+                t.add(column: "createdAt", .date).notNull()
+                // We store this as a blob because we don't currently join on it: status IDs are shown only as a count.
+                t.add(column: "statusIds", .blob).notNull()
+                t.add(column: "targetAccountId").indexed().notNull().references("accountRecord", onDelete: .cascade)
+            }
+        }
+
         return migrator
     }
 }

Views/UIKit/Content Views/NotificationReportView.swift
@@ -1,0 +1,218 @@
+// Copyright © 2023 Vyr Cossont. All rights reserved.
+
+import Foundation
+import Mastodon
+import SDWebImage
+import UIKit
+import ViewModels
+
+private let targetAvatarDimension: CGFloat = .avatarDimension / 2
+
+/// Display the report info that comes with a report notification, including a subset of the target account info.
+class NotificationReportView: UIView {
+    private let stackView = UIStackView()
+    private let targetAccountStackView = UIStackView()
+    private let targetAccountAvatarImageView = SDAnimatedImageView()
+    private let targetAccountDisplayNameLabel = AnimatedAttachmentLabel()
+    private let targetAccountAccountLabel = UILabel()
+    private let categoryStackView = UIStackView()
+    private let categoryIcon = UIImageView()
+    private let categoryLabel = UILabel()
+    private let ruleStackView = UIStackView()
+    private let commentLabel = UILabel()
+
+    init() {
+        super.init(frame: .zero)
+        initialSetup()
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func initialSetup() {
+        addSubview(stackView)
+        stackView.axis = .vertical
+        stackView.spacing = .compactSpacing
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+
+        stackView.addArrangedSubview(targetAccountStackView)
+        targetAccountStackView.axis = .horizontal
+        targetAccountStackView.spacing = .compactSpacing
+
+        stackView.setCustomSpacing(.defaultSpacing, after: targetAccountStackView)
+
+        let targetAccountAvatarStackView = UIStackView()
+        targetAccountStackView.addArrangedSubview(targetAccountAvatarStackView)
+        targetAccountAvatarStackView.axis = .vertical
+
+        targetAccountAvatarStackView.addArrangedSubview(UIView())
+
+        targetAccountAvatarStackView.addArrangedSubview(targetAccountAvatarImageView)
+        targetAccountAvatarImageView.layer.cornerRadius = targetAvatarDimension / 2
+        targetAccountAvatarImageView.clipsToBounds = true
+        targetAccountAvatarImageView.translatesAutoresizingMaskIntoConstraints = false
+
+        targetAccountAvatarStackView.addArrangedSubview(UIView())
+
+        let targetAccountNameStackView = UIStackView()
+        targetAccountStackView.addArrangedSubview(targetAccountNameStackView)
+        targetAccountNameStackView.axis = .vertical
+        targetAccountNameStackView.spacing = .compactSpacing
+
+        targetAccountNameStackView.addArrangedSubview(targetAccountDisplayNameLabel)
+        targetAccountDisplayNameLabel.numberOfLines = 0
+        targetAccountDisplayNameLabel.font = .preferredFont(forTextStyle: .headline)
+        targetAccountDisplayNameLabel.adjustsFontForContentSizeCategory = true
+        targetAccountDisplayNameLabel.textColor = .secondaryLabel
+
+        targetAccountNameStackView.addArrangedSubview(targetAccountAccountLabel)
+        targetAccountAccountLabel.numberOfLines = 0
+        targetAccountAccountLabel.font = .preferredFont(forTextStyle: .subheadline)
+        targetAccountAccountLabel.adjustsFontForContentSizeCategory = true
+        targetAccountAccountLabel.textColor = .secondaryLabel
+
+        stackView.addArrangedSubview(categoryStackView)
+        categoryStackView.axis = .horizontal
+        categoryStackView.spacing = .compactSpacing
+
+        categoryStackView.addArrangedSubview(categoryIcon)
+        categoryIcon.tintColor = .systemOrange
+        categoryIcon.adjustsImageSizeForAccessibilityContentSizeCategory = true
+        categoryIcon.setContentHuggingPriority(.required, for: .horizontal)
+        categoryIcon.setContentCompressionResistancePriority(.required, for: .horizontal)
+
+        categoryStackView.addArrangedSubview(categoryLabel)
+        categoryLabel.font = .preferredFont(forTextStyle: .headline)
+        categoryLabel.adjustsFontForContentSizeCategory = true
+        categoryLabel.textColor = .secondaryLabel
+
+        stackView.addArrangedSubview(ruleStackView)
+        ruleStackView.axis = .vertical
+        ruleStackView.spacing = .compactSpacing
+
+        stackView.setCustomSpacing(.defaultSpacing, after: ruleStackView)
+
+        stackView.addArrangedSubview(commentLabel)
+        commentLabel.numberOfLines = 0
+        commentLabel.font = .preferredFont(forTextStyle: .subheadline)
+        commentLabel.adjustsFontForContentSizeCategory = true
+        commentLabel.textColor = .secondaryLabel
+
+        NSLayoutConstraint.activate([
+            targetAccountAvatarImageView.widthAnchor.constraint(equalToConstant: targetAvatarDimension),
+            targetAccountAvatarImageView.heightAnchor.constraint(equalToConstant: targetAvatarDimension),
+            targetAccountAvatarImageView.centerYAnchor.constraint(equalTo: targetAccountNameStackView.centerYAnchor),
+            stackView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            stackView.topAnchor.constraint(equalTo: topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+
+    public static func estimatedHeight(width: CGFloat, account: Account, comment: String, rules: [Rule]) -> CGFloat {
+        let nameWidth = width - .compactSpacing - targetAvatarDimension
+
+        let nameHeight = account.displayName.height(width: nameWidth, font: .preferredFont(forTextStyle: .headline))
+            + .compactSpacing
+            + account.displayName.height(width: nameWidth, font: .preferredFont(forTextStyle: .subheadline))
+
+        let commentHeight = comment.isEmpty
+            ? 0
+            : comment.height(width: width, font: .preferredFont(forTextStyle: .subheadline))
+                + .compactSpacing
+
+        return max(nameHeight, targetAvatarDimension)
+            + .compactSpacing
+            + UIFont.preferredFont(forTextStyle: .headline).lineHeight
+            + commentHeight
+            + .compactSpacing
+            + CGFloat(rules.count) * (
+                UIFont.preferredFont(forTextStyle: .subheadline).lineHeight
+                + .compactSpacing
+            )
+    }
+
+    public var viewModel: NotificationReportViewModel? {
+        didSet {
+            guard let viewModel = viewModel else {
+                return
+            }
+
+            let accessibilityAttributedLabel = NSMutableAttributedString(string: "")
+
+            let identityContext = viewModel.identityContext
+            let targetAccount = viewModel.report.targetAccount
+            let displayName = targetAccount.displayName
+
+            let avatarUrl = identityContext.appPreferences.animateAvatars == .everywhere
+                ? targetAccount.avatar.url
+                : targetAccount.avatarStatic.url
+            targetAccountAvatarImageView.sd_setImage(with: avatarUrl)
+
+            targetAccountDisplayNameLabel.isHidden = displayName.isEmpty
+            if !displayName.isEmpty {
+                let mutableDisplayName = NSMutableAttributedString(string: displayName)
+                mutableDisplayName.insert(
+                    emojis: targetAccount.emojis,
+                    view: targetAccountDisplayNameLabel,
+                    identityContext: identityContext
+                )
+                mutableDisplayName.resizeAttachments(toLineHeight: targetAccountDisplayNameLabel.font.lineHeight)
+                targetAccountDisplayNameLabel.attributedText = mutableDisplayName
+
+                accessibilityAttributedLabel.appendWithSeparator(displayName)
+            }
+
+            targetAccountAccountLabel.text = "@".appending(targetAccount.acct)
+            accessibilityAttributedLabel.appendWithSeparator("@".appending(targetAccount.acct))
+
+            let category = viewModel.report.category
+            categoryIcon.image = .init(systemName: category.systemImageName)
+            categoryLabel.text = category.title
+            accessibilityAttributedLabel.appendWithSeparator(NSLocalizedString("report.category", comment: ""))
+            accessibilityAttributedLabel.appendWithSeparator(category.title)
+
+            for subview in ruleStackView.arrangedSubviews {
+                subview.removeFromSuperview()
+            }
+
+            for rule in viewModel.rules {
+                let ruleHStackView = UIStackView()
+                ruleStackView.addArrangedSubview(ruleHStackView)
+                ruleHStackView.axis = .horizontal
+                ruleHStackView.spacing = .compactSpacing
+
+                let ruleIcon = UIImageView(
+                    image: .init(
+                        systemName: "xmark.octagon.fill",
+                        withConfiguration: UIImage.SymbolConfiguration(scale: .small)
+                    )
+                )
+                ruleHStackView.addArrangedSubview(ruleIcon)
+                ruleIcon.tintColor = .systemOrange
+                ruleIcon.adjustsImageSizeForAccessibilityContentSizeCategory = true
+
+                let ruleLabel = UILabel()
+                ruleHStackView.addArrangedSubview(ruleLabel)
+                // Intentionally left defaulted to one line because some rules are llllooooonnggg.
+                ruleLabel.font = .preferredFont(forTextStyle: .subheadline)
+                ruleLabel.adjustsFontForContentSizeCategory = true
+                ruleLabel.textColor = .secondaryLabel
+                ruleLabel.text = rule.text
+                // Also intentionally left out of accessibility label for the same reason.
+            }
+            ruleStackView.isHidden = viewModel.rules.isEmpty
+
+            let comment = viewModel.report.comment
+            commentLabel.isHidden = comment.isEmpty
+            commentLabel.text = comment
+            if !comment.isEmpty {
+                accessibilityAttributedLabel.appendWithSeparator(comment)
+            }
+
+            self.accessibilityAttributedLabel = accessibilityAttributedLabel
+        }
+    }
+}

DB/Sources/DB/Content/ContentDatabase.swift
@@ -451,6 +451,13 @@ public extension ContentDatabase {
         }
     }
 
+    func update(instance: Instance) -> AnyPublisher<Never, Error> {
+        databaseWriter.mutatingPublisher {
+            try instance.save($0)
+            try updateRules(rules: instance.rules, db: $0)
+        }
+    }
+
     func update(emojis: [Emoji]) -> AnyPublisher<Never, Error> {
         databaseWriter.mutatingPublisher {
             for emoji in emojis {
@@ -482,6 +489,20 @@ public extension ContentDatabase {
         }
     }
 
+    func update(rules: [Rule]) -> AnyPublisher<Never, Error> {
+        databaseWriter.mutatingPublisher {
+            try updateRules(rules: rules, db: $0)
+        }
+    }
+
+    private func updateRules(rules: [Rule], db: Database) throws {
+        for rule in rules {
+            try rule.save(db)
+        }
+
+        try Rule.filter(!rules.map(\.id).contains(Rule.Columns.id)).deleteAll(db)
+    }
+
     func insert(results: Results) -> AnyPublisher<Never, Error> {
         databaseWriter.mutatingPublisher {
             for account in results.accounts {
@@ -492,10 +513,6 @@ public extension ContentDatabase {
                 try status.save($0)
             }
         }
-    }
-
-    func insert(instance: Instance) -> AnyPublisher<Never, Error> {
-        databaseWriter.mutatingPublisher(updates: instance.save)
     }
 
     func timelinePublisher(_ timeline: Timeline) -> AnyPublisher<[CollectionSection], Error> {
@@ -659,7 +676,8 @@ public extension ContentDatabase {
     }
 
     func notificationsPublisher(
-        excludeTypes: Set<MastodonNotification.NotificationType>) -> AnyPublisher<[CollectionSection], Error> {
+        excludeTypes: Set<MastodonNotification.NotificationType>
+    ) -> AnyPublisher<[CollectionSection], Error> {
         ValueObservation.tracking(
             NotificationInfo.request(
                 NotificationRecord.order(NotificationRecord.Columns.createdAt.desc)
@@ -677,7 +695,9 @@ public extension ContentDatabase {
                     configuration = nil
                 }
 
-                return .notification(MastodonNotification(info: $0), configuration)
+                let rules = $0.reportInfo?.rules ?? []
+
+                return .notification(MastodonNotification(info: $0), rules, configuration)
             })] }
             .eraseToAnyPublisher()
     }
@@ -693,13 +713,14 @@ public extension ContentDatabase {
             .eraseToAnyPublisher()
     }
 
-    func instancePublisher(uri: String) -> AnyPublisher<Instance, Error> {
+    func instancePublisher() -> AnyPublisher<Instance, Error> {
         ValueObservation.tracking(
-            InstanceInfo.request(InstanceRecord.filter(InstanceRecord.Columns.uri == uri)).fetchOne)
+            InstanceInfo.request(InstanceRecord.all()).fetchOne)
             .removeDuplicates()
             .publisher(in: databaseWriter)
             .compactMap { $0 }
-            .map(Instance.init(info:))
+            .combineLatest(rulesPublisher())
+            .map { Instance(info: $0, rules: $1) }
             .eraseToAnyPublisher()
     }
 
@@ -734,6 +755,13 @@ public extension ContentDatabase {
 
     func emojiUses(limit: Int) -> AnyPublisher<[EmojiUse], Error> {
         databaseWriter.readPublisher(value: EmojiUse.all().order(EmojiUse.Columns.count.desc).limit(limit).fetchAll)
+            .eraseToAnyPublisher()
+    }
+
+    func rulesPublisher() -> AnyPublisher<[Rule], Error> {
+        ValueObservation.tracking(Rule.fetchAll)
+            .removeDuplicates()
+            .publisher(in: databaseWriter)
             .eraseToAnyPublisher()
     }
 

DB/Sources/DB/Content/InstanceRecord.swift
@@ -20,7 +20,6 @@ struct InstanceRecord: ContentDatabaseRecord, Hashable {
     let thumbnail: UnicodeURL?
     let contactAccountId: Account.Id?
     let maxTootChars: Int?
-    let rules: [Rule]?
 }
 
 extension InstanceRecord {
@@ -40,7 +39,6 @@ extension InstanceRecord {
         static let thumbnail = Column(CodingKeys.thumbnail)
         static let contactAccountId = Column(CodingKeys.contactAccountId)
         static let maxTootChars = Column(CodingKeys.maxTootChars)
-        static let rules = Column(CodingKeys.rules)
     }
 
     static let contactAccount = belongsTo(AccountRecord.self)
@@ -61,6 +59,5 @@ extension InstanceRecord {
         self.thumbnail = instance.thumbnail
         self.contactAccountId = instance.contactAccount?.id
         self.maxTootChars = instance.maxTootChars
-        self.rules = instance.rules
     }
 }

DB/Sources/DB/Content/NotificationInfo.swift
@@ -7,7 +7,7 @@ struct NotificationInfo: Codable, Hashable, FetchableRecord {
     let record: NotificationRecord
     let accountInfo: AccountInfo
     let statusInfo: StatusInfo?
-    let reportRecord: ReportRecord?
+    let reportInfo: ReportInfo?
 }
 
 extension NotificationInfo {
@@ -16,7 +16,8 @@ extension NotificationInfo {
                             .forKey(CodingKeys.accountInfo))
             .including(optional: StatusInfo.addingIncludesForNotificationInfo(NotificationRecord.status)
                         .forKey(CodingKeys.statusInfo))
-            .including(optional: NotificationRecord.report.forKey(CodingKeys.reportRecord))
+            .including(optional: ReportInfo.addingIncludesForNotificationInfo(NotificationRecord.report)
+                .forKey(CodingKeys.reportInfo))
     }
 
     static func request(_ request: QueryInterfaceRequest<NotificationRecord>) -> QueryInterfaceRequest<Self> {

DB/Sources/DB/Content/ReportInfo.swift
@@ -1,0 +1,45 @@
+// Copyright © 2023 Vyr Cossont. All rights reserved.
+
+import Foundation
+import GRDB
+import Mastodon
+
+struct ReportInfo: Codable, Hashable, FetchableRecord {
+    let record: ReportRecord
+    let rules: [Rule]
+    let targetAccountInfo: AccountInfo
+}
+
+extension ReportInfo {
+    static func addingIncludes<T: DerivableRequest>(_ request: T) -> T where T.RowDecoder == ReportRecord {
+        addingOptionalIncludes(
+            request
+                .including(
+                    required: AccountInfo.addingIncludes(ReportRecord.targetAccount)
+                        .forKey(CodingKeys.targetAccountInfo)
+                )
+        )
+    }
+
+    // Hack, remove once GRDB supports chaining a required association behind an optional association
+    static func addingIncludesForNotificationInfo<T: DerivableRequest>(
+        _ request: T) -> T where T.RowDecoder == ReportRecord {
+        addingOptionalIncludes(
+            request
+                .including(
+                    optional: AccountInfo.addingIncludes(ReportRecord.targetAccount)
+                        .forKey(CodingKeys.targetAccountInfo)
+                )
+        )
+    }
+
+    private static func addingOptionalIncludes<T: DerivableRequest>(
+        _ request: T) -> T where T.RowDecoder == ReportRecord {
+        request
+            .including(all: ReportRecord.rules.forKey(CodingKeys.rules))
+    }
+
+    static func request(_ request: QueryInterfaceRequest<ReportRecord>) -> QueryInterfaceRequest<Self> {
+        addingIncludes(request).asRequest(of: self)
+    }
+}

DB/Sources/DB/Content/ReportRecord.swift
@@ -7,16 +7,45 @@ import Mastodon
 struct ReportRecord: ContentDatabaseRecord, Hashable {
     let id: Report.Id
     let actionTaken: Bool
+    let actionTakenAt: Date?
+    let category: Report.Category
+    let comment: String
+    let forwarded: Bool
+    let createdAt: Date
+    let statusIds: [Status.Id]
+    let targetAccountId: Account.Id
 }
 
 extension ReportRecord {
     enum Columns {
         static let id = Column(CodingKeys.id)
         static let actionTaken = Column(CodingKeys.actionTaken)
+        static let actionTakenAt = Column(CodingKeys.actionTakenAt)
+        static let category = Column(CodingKeys.category)
+        static let comment = Column(CodingKeys.comment)
+        static let forwarded = Column(CodingKeys.forwarded)
+        static let createdAt = Column(CodingKeys.createdAt)
+        static let statusIds = Column(CodingKeys.statusIds)
+        static let targetAccountId = Column(CodingKeys.targetAccountId)
     }
+
+    static let ruleJoins = hasMany(ReportRuleJoin.self)
+    static let rules = hasMany(
+        Rule.self,
+        through: ruleJoins,
+        using: ReportRuleJoin.rule
+    )
+    static let targetAccount = belongsTo(AccountRecord.self)
 
     init(report: Report) {
         id = report.id
         actionTaken = report.actionTaken
+        actionTakenAt = report.actionTakenAt
+        category = report.category
+        comment = report.comment
+        forwarded = report.forwarded
+        createdAt = report.createdAt
+        statusIds = report.statusIds ?? []
+        targetAccountId = report.targetAccount.id
     }
 }

DB/Sources/DB/Content/ReportRuleJoin.swift
@@ -1,0 +1,19 @@
+// Copyright © 2023 Vyr Cossont. All rights reserved.
+
+import Foundation
+import GRDB
+import Mastodon
+
+struct ReportRuleJoin: ContentDatabaseRecord {
+    let reportId: Report.Id
+    let ruleId: Rule.Id
+}
+
+extension ReportRuleJoin {
+    enum Columns {
+        static let reportId = Column(CodingKeys.reportId)
+        static let ruleId = Column(CodingKeys.ruleId)
+    }
+
+    static let rule = belongsTo(Rule.self, using: ForeignKey([Columns.ruleId]))
+}

commit b160d9872357de9865ec9364abb7ca9fab665ca9

Fix DB migration and animation issues introduced in 3ac900f2d614f47aa814b3189ce07bab4df7dadd

DB/Sources/DB/Content/ContentDatabase+Migration.swift
@@ -352,9 +352,11 @@ extension ContentDatabase {
                 t.column("text", .text).notNull()
             }
 
+            // Delete all stored notifications because we're about to mess with a table that they join to.
+            try db.execute(sql: "DELETE FROM notificationRecord")
+
             // Note: it's possible for a report notification to refer to statuses or rules that no longer exist,
             // so we don't enforce a foreign key constraint on `ruleId`.
-
             try db.create(table: "reportRuleJoin") { t in
                 t.column("reportId", .text).indexed().notNull()
                     .references("reportRecord", onDelete: .cascade)
@@ -363,15 +365,21 @@ extension ContentDatabase {
                 t.primaryKey(["reportId", "ruleId"], onConflict: .replace)
             }
 
-            try db.alter(table: "reportRecord") { t in
-                t.add(column: "actionTakenAt", .date)
-                t.add(column: "category", .text).notNull()
-                t.add(column: "comment", .text).notNull()
-                t.add(column: "forwarded", .boolean).notNull()
-                t.add(column: "createdAt", .date).notNull()
+            // Drop and re-create this table because we're adding a bunch of non-nullable columns without defaults,
+            // which SQLite won't let you do (even if the table is empty, which it might not be).
+            // See https://www.sqlite.org/lang_altertable.html#alter_table_add_column
+            try db.drop(table: "reportRecord")
+            try db.create(table: "reportRecord") { t in
+                t.column("id", .text).primaryKey(onConflict: .replace)
+                t.column("actionTaken", .boolean).notNull()
+                t.column("actionTakenAt", .date)
+                t.column("category", .text).notNull()
+                t.column("comment", .text).notNull()
+                t.column("forwarded", .boolean).notNull()
+                t.column("createdAt", .date).notNull()
                 // We store this as a blob because we don't currently join on it: status IDs are shown only as a count.
-                t.add(column: "statusIds", .blob).notNull()
-                t.add(column: "targetAccountId").indexed().notNull().references("accountRecord", onDelete: .cascade)
+                t.column("statusIds", .blob).notNull()
+                t.column("targetAccountId").indexed().notNull().references("accountRecord", onDelete: .cascade)
             }
         }
 

Views/UIKit/Content Views/NotificationReportView.swift
@@ -18,6 +18,7 @@ class NotificationReportView: UIView {
     private let categoryStackView = UIStackView()
     private let categoryIcon = UIImageView()
     private let categoryLabel = UILabel()
+    private var rules = [Rule]()
     private let ruleStackView = UIStackView()
     private let commentLabel = UILabel()
 
@@ -174,36 +175,42 @@ class NotificationReportView: UIView {
             accessibilityAttributedLabel.appendWithSeparator(NSLocalizedString("report.category", comment: ""))
             accessibilityAttributedLabel.appendWithSeparator(category.title)
 
-            for subview in ruleStackView.arrangedSubviews {
-                subview.removeFromSuperview()
-            }
+            if rules != viewModel.rules {
+                // We guard these view changes because the view model seems to be assigned to many times during
+                // the animation when a notification is tapped, causing flashes of wrong colors or out of place views.
+                rules = viewModel.rules
 
-            for rule in viewModel.rules {
-                let ruleHStackView = UIStackView()
-                ruleStackView.addArrangedSubview(ruleHStackView)
-                ruleHStackView.axis = .horizontal
-                ruleHStackView.spacing = .compactSpacing
+                for subview in ruleStackView.arrangedSubviews {
+                    subview.removeFromSuperview()
+                }
 
-                let ruleIcon = UIImageView(
-                    image: .init(
-                        systemName: "xmark.octagon.fill",
-                        withConfiguration: UIImage.SymbolConfiguration(scale: .small)
+                for rule in viewModel.rules {
+                    let ruleHStackView = UIStackView()
+                    ruleStackView.addArrangedSubview(ruleHStackView)
+                    ruleHStackView.axis = .horizontal
+                    ruleHStackView.spacing = .compactSpacing
+
+                    let ruleIcon = UIImageView(
+                        image: .init(
+                            systemName: "xmark.octagon.fill",
+                            withConfiguration: UIImage.SymbolConfiguration(scale: .small)
+                        )
                     )
-                )
-                ruleHStackView.addArrangedSubview(ruleIcon)
-                ruleIcon.tintColor = .systemOrange
-                ruleIcon.adjustsImageSizeForAccessibilityContentSizeCategory = true
+                    ruleHStackView.addArrangedSubview(ruleIcon)
+                    ruleIcon.tintColor = .systemOrange
+                    ruleIcon.adjustsImageSizeForAccessibilityContentSizeCategory = true
 
-                let ruleLabel = UILabel()
-                ruleHStackView.addArrangedSubview(ruleLabel)
-                // Intentionally left defaulted to one line because some rules are llllooooonnggg.
-                ruleLabel.font = .preferredFont(forTextStyle: .subheadline)
-                ruleLabel.adjustsFontForContentSizeCategory = true
-                ruleLabel.textColor = .secondaryLabel
-                ruleLabel.text = rule.text
-                // Also intentionally left out of accessibility label for the same reason.
+                    let ruleLabel = UILabel()
+                    ruleHStackView.addArrangedSubview(ruleLabel)
+                    // Intentionally left defaulted to one line because some rules are llllooooonnggg.
+                    ruleLabel.font = .preferredFont(forTextStyle: .subheadline)
+                    ruleLabel.adjustsFontForContentSizeCategory = true
+                    ruleLabel.textColor = .secondaryLabel
+                    ruleLabel.text = rule.text
+                    // Also intentionally left out of accessibility label for the same reason.
+                }
+                ruleStackView.isHidden = viewModel.rules.isEmpty
             }
-            ruleStackView.isHidden = viewModel.rules.isEmpty
 
             let comment = viewModel.report.comment
             commentLabel.isHidden = comment.isEmpty